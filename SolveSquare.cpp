89c2c55e2c338d97db858e952a899b140ce3770a
fokrey <67243838+fokrey@users.noreply.github.com>
2020-09-27 13:10:33 +0300

    Update SolveSquare.cpp

--- SolveSquare.cpp
+++ SolveSquare.cpp
@@ -38,7 +38,6 @@ int SolveSquare (double a, double b, double c, double* x1, double* x2) {
     return NO_ROOTS;
 }
 
-
 //---------------------------------------------------------
 /** \brief This function solves Linear Equation \f$ ax + b = 0 \f$
     \param [in] a The first  coefficient
